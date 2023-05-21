5023d490a0570a9ce3c98e96f19f9149ebdf0438
AyoubMDL <ayoubbih119@gmail.com>
2023-05-21 17:02:08 +0200

    create the flask web app

---
+++ application.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+application = Flask(__name__)
+
+@application.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            brand = request.form['brand'],
+            processor_brand = request.form['processor_brand'],
+            processor_name = request.form['processor_name'],
+            processor_gnrtn = request.form['processor_gnrtn'],
+            ram_gb = request.form['ram_gb'],
+            ram_type = request.form['ram_type'],
+            ssd = request.form['ssd'],
+            hdd = request.form['hdd'],
+            os = request.form['os'],
+            os_bit = request.form['os_bit'],
+            graphic_card_gb = request.form['graphic_card_gb'],
+            weight = request.form['weight'],
+            warranty = request.form['warranty'],
+            touchscreen = request.form['Touchscreen'],
+            msoffice = request.form['msoffice'],
+            rating = request.form['rating']
+        )
+
+        pred_df = data.get_data_as_data_frame()
+        prediction_pipeline = PredictPipeline()
+        prediction = prediction_pipeline.predict(pred_df)
+
+        return render_template("home.html", results=round(prediction[0], 2))
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,86 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = "artifacts/model/model.pkl"
+            preprocessor_path = "artifacts/preprocessor/preprocessor.pkl"
+            model = load_object(model_path)
+            preprocessor = load_object(preprocessor_path)
+
+            data_preprocessed = preprocessor.transform(features)
+            prediction = model.predict(data_preprocessed)
+
+            return prediction
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+class CustomData:
+    def __init__(self,
+                 brand: str,
+                 processor_brand: str,
+                 processor_name: str,
+                 processor_gnrtn: str,
+                 ram_gb: str,
+                 ram_type: str,
+                 ssd: str,
+                 hdd: str,
+                 os: str,
+                 os_bit: str,
+                 graphic_card_gb: str,
+                 weight: str,
+                 warranty: str,
+                 touchscreen: str,
+                 msoffice: str,
+                 rating: str):
+        
+        self.brand = brand
+        self.processor_brand = processor_brand
+        self.processor_name = processor_name
+        self.processor_gnrtn = processor_gnrtn
+        self.ram_gb = ram_gb
+        self.ram_type = ram_type
+        self.ssd = ssd
+        self.hdd = hdd
+        self.os = os
+        self.os_bit = os_bit
+        self.graphic_card_gb = graphic_card_gb
+        self.weight = weight
+        self.warranty = warranty
+        self.touchscreen = touchscreen
+        self.msoffice = msoffice
+        self.rating = rating
+        
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "brand": [self.brand],
+                "processor_brand": [self.processor_brand],
+                "processor_name": [self.processor_name],
+                "processor_gnrtn": [self.processor_gnrtn],
+                "ram_gb": [self.ram_gb],
+                "ram_type": [self.ram_type],
+                "ssd": [self.ssd],
+                "hdd": [self.hdd],
+                "os": [self.os],
+                "os_bit": [self.os_bit],
+                "graphic_card_gb": [self.graphic_card_gb],
+                "weight": [self.weight],
+                "warranty": [self.warranty],
+                "Touchscreen": [self.touchscreen],
+                "msoffice": [self.msoffice],
+                "rating": [self.rating],
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
