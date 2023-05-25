695ecdf84fa2280a67ed494ace6128cf6dbf7ac1
Ayoub BIH <89558574+AyoubMDL@users.noreply.github.com>
2023-05-25 17:05:42 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,10 @@
+import logging
 from flask import Flask, render_template, request
+
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 
 application = Flask(__name__)
 
@@ -8,31 +13,37 @@ def index():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        data = CustomData(
-            brand = request.form['brand'],
-            processor_brand = request.form['processor_brand'],
-            processor_name = request.form['processor_name'],
-            processor_gnrtn = request.form['processor_gnrtn'],
-            ram_gb = request.form['ram_gb'],
-            ram_type = request.form['ram_type'],
-            ssd = request.form['ssd'],
-            hdd = request.form['hdd'],
-            os = request.form['os'],
-            os_bit = request.form['os_bit'],
-            graphic_card_gb = request.form['graphic_card_gb'],
-            weight = request.form['weight'],
-            warranty = request.form['warranty'],
-            touchscreen = request.form['Touchscreen'],
-            msoffice = request.form['msoffice'],
-            rating = request.form['rating']
-        )
+        try:
+            data = CustomData(
+                brand=request.form['brand'],
+                processor_brand=request.form['processor_brand'],
+                processor_name=request.form['processor_name'],
+                processor_gnrtn=request.form['processor_gnrtn'],
+                ram_gb=request.form['ram_gb'],
+                ram_type=request.form['ram_type'],
+                ssd=request.form['ssd'],
+                hdd=request.form['hdd'],
+                os=request.form['os'],
+                os_bit=request.form['os_bit'],
+                graphic_card_gb=request.form['graphic_card_gb'],
+                weight=request.form['weight'],
+                warranty=request.form['warranty'],
+                touchscreen=request.form['Touchscreen'],
+                msoffice=request.form['msoffice'],
+                rating=request.form['rating']
+            )
 
-        pred_df = data.get_data_as_data_frame()
-        prediction_pipeline = PredictPipeline()
-        prediction = prediction_pipeline.predict(pred_df)
+            pred_df = data.get_data_as_data_frame()
+            prediction_pipeline = PredictPipeline()
+            prediction = prediction_pipeline.predict(pred_df)
 
-        return render_template("home.html", results=round(prediction[0], 2))
+            return render_template("home.html", results=round(prediction[0], 2))
 
+        except Exception as e:
+            # Log the exception with detailed message
+            logging.exception("An error occurred during request processing: %s", str(e))
+            # Render an error page or display a user-friendly error message
+            return render_template("error.html", error_message="An internal server error occurred: {}".format(str(e)))
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=8080)
